def273e03ca708c04c703b5feeda89a50c16b204
Raido Raagmaa <raido.raagmaa@nortal.com>
2024-06-28 11:06:02 +0300

    SIVA-684 Decrease tests console logging

--- src/main/groovy/ee/openid/siva/test/BeforeAll.groovy
+++ src/main/groovy/ee/openid/siva/test/BeforeAll.groovy
@@ -2,16 +2,16 @@ package ee.openid.siva.test
 
 import ee.openid.siva.test.util.AllureRestAssuredWithStep
 import io.restassured.RestAssured
-import io.restassured.filter.log.RequestLoggingFilter
-import io.restassured.filter.log.ResponseLoggingFilter
 
 class BeforeAll {
 
     BeforeAll() {
 
         // Rest Assured settings
-        // Log all requests and responses locally and in allure report
-        RestAssured.filters(new AllureRestAssuredWithStep(), new RequestLoggingFilter(), new ResponseLoggingFilter())
+        // Log all requests and responses in allure report
+        RestAssured.filters(new AllureRestAssuredWithStep())
+        // Limit console logging to failed tests only
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
         // Relax validation
         RestAssured.useRelaxedHTTPSValidation()
     }
