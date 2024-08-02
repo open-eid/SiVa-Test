3f75cee9c5a4424c8681d11ac11a0ea338bd6596
Raido Raagmaa <raido.raagmaa@nortal.com>
2024-08-02 11:50:45 +0300

    SIVA-691 Add copyright headers

--- src/main/groovy/ee/openeid/siva/test/util/TestDescriptionExtension.groovy
+++ src/main/groovy/ee/openeid/siva/test/util/TestDescriptionExtension.groovy
@@ -1,4 +1,21 @@
+/*
+ * Copyright 2024 - 2024 Riigi Infosüsteemi Amet
+ *
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL (the "Licence")
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/software/page/eupl
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
 package ee.openeid.siva.test.util
+
+import io.qameta.allure.Description
 import org.spockframework.runtime.extension.IGlobalExtension
 import org.spockframework.runtime.extension.IMethodInterceptor
 import org.spockframework.runtime.extension.IMethodInvocation
@@ -39,15 +56,26 @@ class TestDescriptionExtension implements IGlobalExtension {
     }
 
     void captureTestDescription(FeatureInfo feature, MethodInfo method, String filename) {
+        // Add test case name
         def description = "### Test Case: ${feature.name}\n\n"
+
+        // Add Allure description if present
+        def methodReflection = method.reflection
+        if (methodReflection.isAnnotationPresent(Description)) {
+            Description allureDescription = methodReflection.getAnnotation(Description)
+            description += "**Description:** ${allureDescription.value()}\n\n"
+        }
+
+        // Add test stages description
         feature.blocks.each { block ->
             def blockText = block.texts.join(" ")
             description += "**${block.kind.name().capitalize()}:** ${blockText}\n\n"
         }
+
         if (!testDescriptions.containsKey(filename)) {
             testDescriptions[filename] = []
         }
-        testDescriptions[filename] << description
+        testDescriptions[filename] << (description as String)
     }
 
     void writeTestDescriptionsToFile() {
