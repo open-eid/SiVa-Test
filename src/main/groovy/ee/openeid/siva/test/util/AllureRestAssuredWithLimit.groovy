6df3f079edc399b4252b22e1e557af5f8c59f563
Raido Raagmaa <raido.raagmaa@nortal.com>
2024-08-13 10:39:14 +0300

    SIVA-702 arranging licenses for AllureRestAssuredWithLimit

--- src/main/groovy/ee/openeid/siva/test/util/AllureRestAssuredWithLimit.groovy
+++ src/main/groovy/ee/openeid/siva/test/util/AllureRestAssuredWithLimit.groovy
@@ -13,6 +13,31 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the Licence for the specific language governing permissions and limitations under the Licence.
  */
+
+/*
+ *  Copyright 2016-2024 Qameta Software Inc
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*
+ * Original work licensed under the Apache License, Version 2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Modifications  licensed under the EUPL, Version 1.1 or – as soon they will be approved by
+ * the European Commission - subsequent versions of the EUPL
+ * https://joinup.ec.europa.eu/software/page/eupl
+ */
 package ee.openeid.siva.test.util
 
 import ee.openeid.siva.test.ConfigHolder
