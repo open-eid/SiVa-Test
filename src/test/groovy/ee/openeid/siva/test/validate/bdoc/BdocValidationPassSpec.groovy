cf2b5df4cf40c92e3d166f057f6df7163de3194c
Raido Raagmaa <raido.raagmaa@nortal.com>
2025-04-10 18:09:54 +0300

    SIVA-780 Add timestampCreationTime field checks

--- src/test/groovy/ee/openeid/siva/test/validate/bdoc/BdocValidationPassSpec.groovy
+++ src/test/groovy/ee/openeid/siva/test/validate/bdoc/BdocValidationPassSpec.groovy
@@ -27,7 +27,6 @@ import ee.openeid.siva.test.request.SivaRequests
 import io.qameta.allure.Description
 import spock.lang.Ignore
 
-import static ee.openeid.siva.test.TestData.getVALIDATION_CONCLUSION_PREFIX
 import static org.hamcrest.Matchers.*
 
 class BdocValidationPassSpec extends GenericSpecification {
@@ -36,7 +35,7 @@ class BdocValidationPassSpec extends GenericSpecification {
     def "Given validation request with BDOC #profile signature, then validation report is returned"() {
         expect:
         SivaRequests.validate(RequestData.validationRequest(file))
-                .then().rootPath(VALIDATION_CONCLUSION_PREFIX)
+                .then().rootPath(TestData.VALIDATION_CONCLUSION_PREFIX)
                 .body("signatureForm", equalTo(ContainerFormat.ASiC_E))
                 .body("validatedDocument.filename", equalTo(file))
                 .body("signatures[0].signatureFormat", is(profile))
@@ -342,5 +341,30 @@ class BdocValidationPassSpec extends GenericSpecification {
                 ))
                 .body("signaturesCount", is(1))
                 .body("validSignaturesCount", is(1))
+    }
+
+    @Description("Simple report includes timestamp creation time for timestamped signature")
+    def "Given BDOC with timestamped signature, then validation report includes timestampCreationTime field"() {
+        expect:
+        SivaRequests.validate(RequestData.validationRequest(file, "bdoc"))
+                .then().rootPath(TestData.VALIDATION_CONCLUSION_PREFIX)
+                .body("signatureForm", equalTo(ContainerFormat.ASiC_E))
+                .body("signatures[0].info.timestampCreationTime", is(timestampCreationTime))
+
+        where:
+        file                                   | timestampCreationTime
+        "TEST_ESTEID2018_ASiC-E_XAdES_T.sce"   | "2024-09-13T14:14:24Z"
+        "TEST_ESTEID2018_ASiC-E_XAdES_LT.sce"  | "2024-09-13T14:14:36Z"
+        "TEST_ESTEID2018_ASiC-E_XAdES_LTA.sce" | "2024-09-13T14:14:47Z"
+    }
+
+    @Description("Simple report includes timestamp creation time for timestamped signature")
+    def "Given BDOC with multiple timestamped signatures, then validation report includes timestampCreationTime field for each"() {
+        expect:
+        SivaRequests.validate(RequestData.validationRequest("3_signatures_TM_LT_LTA.sce", "bdoc"))
+                .then().rootPath(TestData.VALIDATION_CONCLUSION_PREFIX)
+                .body("signatureForm", equalTo(ContainerFormat.ASiC_E))
+                .body("signatures[1].info.timestampCreationTime", is("2021-01-29T14:31:36Z"))
+                .body("signatures[2].info.timestampCreationTime", is("2021-01-29T14:38:11Z"))
     }
 }
